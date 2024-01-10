bf3652e30b820317a08a61dabfd172ff92c249f2
parfaitandre5@gmail.com <parfaitandre5@gmail.com>
2024-01-10 11:20:44 +0100

    server test commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require("http")
+const app = http.createServer((req, res) =>{
+    res.writeHead(200, {"Content-Type": "text/plain"})
+    res.end("hello world!")
+})
+
+const port = process.env.PORT || 3005;
+
+app.listen(port);
+console.log("started at port ",port);
